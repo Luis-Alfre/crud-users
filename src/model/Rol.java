cdc3f85da86057e999a873086775c7b7bc7b8efe
Alexander <alexsanoso@gmail.com>
2024-05-30 18:36:45 -0500

    tablas modelo rol y user

---
+++ src/model/Rol.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Rol {
+    private int id;
+    private String name;
+
+    // Constructor
+    public Rol(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Rol() {
+    }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
